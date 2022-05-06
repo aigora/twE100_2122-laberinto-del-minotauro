a36a08e89d54e5ca7507eb050647f989207a86be
Davidd02 <david.gomez52@alumnos.upm.es>
2022-05-06 18:58:14 +0200

    usuario

--- TRABAJO/codigo.c
+++ TRABAJO/codigo.c
@@ -1,1 +1,8 @@
 #include<stdio.h>
+int main()
+{
+    char usuario[50];
+    printf("Para empezar, elige un nombre de usuario con el que nos referiremos a ti\n");
+    scanf("%50[^\n]",usuario);
+    printf("Bienvenido,encantado de conocerte %s",usuario);
+}
